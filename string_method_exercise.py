2544bd5da4d48fa67a1e084f3bc4d797325caf87
junojr <45982867+junojr@users.noreply.github.com>
2019-10-27 18:45:03 -0400

    Add files via upload

---
+++ string_method_exercise.py
@@ -0,0 +1,13 @@
+# 11.12 Exercise one
+school = 'countryside elementary school'
+help(str)
+# 11.12 Exercise two
+country = 'usa'
+correct_country = country.upper()
+print(correct_country)
+# 11.12 exercise three
+filename = "hello.py"
+print(filename.endswith(".java"))
+print(filename.endswith(".py"))
+print(filename.startswith("world"))
+
